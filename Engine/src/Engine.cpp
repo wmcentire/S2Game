62f4e2adc74cb0ec7c7c650a34c49219c3ad106b
wmcentire <wmcentire@student.neumont.edu>
2022-08-16 09:14:42 -0600

    Alright, so now Texture is complaining. IDK why, but it is.

--- Engine/src/Engine.cpp
+++ Engine/src/Engine.cpp
@@ -10,7 +10,7 @@ namespace pb {
 	void Engine::Register() {
 		REGISTER_CLASS(Actor);
 		REGISTER_CLASS(AudioComponent);
-		REGISTER_CLASS(ModelComponent);
+		//REGISTER_CLASS(ModelComponent);
 		REGISTER_CLASS(PhysicsComponent);
 		REGISTER_CLASS(PlayerComponent);
 		REGISTER_CLASS(SpriteComponent);
